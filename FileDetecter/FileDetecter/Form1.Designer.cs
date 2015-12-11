e6cac87fab9fdb2e2f820566b338f20ac6c9b7b3
KrAndroid <v6306@naver.com>
2015-12-11 17:12:13 +0900

    C# FileDetecter

---
+++ FileDetecter/FileDetecter/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace FileDetecter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 384);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "경로 :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 384);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 12);
+            this.label2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(96, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 28);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "경로설정(&d)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "경로변경사항";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(13, 38);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(268, 340);
+            this.listBox1.TabIndex = 0;
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            this.fileSystemWatcher1.Created += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Created_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(186, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 28);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "연결프로그램";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 405);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "연결프로그램 경로";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 421);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 12);
+            this.label5.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 443);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "FileDetecter";
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ FileDetecter/FileDetecter/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FileDetecter
+{
+    public partial class Form1 : Form
+    {
+        string Path;
+        string ExePath;
+        FolderBrowserDialog FDB;
+        public Form1()
+        {
+            InitializeComponent();          
+            label2.Text = "설정된 경로 없음";
+            label5.Text = "설정된 경로 없음";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            FDB = new FolderBrowserDialog();
+            // FolderBrowserDialog 생성
+
+            if (FDB.ShowDialog() == DialogResult.OK)
+            //대화상자(Dialog)를 띄운후 대화상자의 응답이 확인이라면,
+            {
+                Path = FDB.SelectedPath;
+                //Path 변수에 선택된 폴더의 경로를 집어넣는다.
+                label2.Text = Path;
+                this.fileSystemWatcher1.Path = Path;
+            }
+        }
+
+        private void fileSystemWatcher1_Created_1(object sender, System.IO.FileSystemEventArgs e)
+        {
+            listBox1.Items.Add(e.Name + "이(가) 해당경로에 생성됨");
+            FileInfo FI = new FileInfo(@ExePath);
+            if (ExePath != null)
+            {
+                if (FI.Exists == true)              
+                    System.Diagnostics.Process.Start(ExePath);
+                if(FI.Exists == false)
+                    MessageBox.Show("연결프로그램이 해당경로에 없거나, 경로가 잘못되었습니다.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog myOpenFilelDialog = new OpenFileDialog();
+            myOpenFilelDialog.DefaultExt = "*.*";
+            myOpenFilelDialog.InitialDirectory = ".";
+            myOpenFilelDialog.Filter = "응용프로그램(*.exe) |*.exe| 모든프로그램(*.*)|*.*";
+            myOpenFilelDialog.FilterIndex = 1;
+            myOpenFilelDialog.RestoreDirectory = true;
+            // FolderBrowserDialog 생성
+ 
+            if (myOpenFilelDialog.ShowDialog() == DialogResult.OK)
+            //대화상자(Dialog)를 띄운후 대화상자의 응답이 확인이라면,
+            {
+                ExePath = myOpenFilelDialog.FileName;
+                //Path 변수에 선택된 폴더의 경로를 집어넣는다.
+                label5.Text = ExePath;
+            }
+        }
+    }
+}
